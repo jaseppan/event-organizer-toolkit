df43c9fac2347be3bd778a13535df88c671c6eaa
Janne Seppänen <j.v.seppanen@gmail.com>
2023-05-19 13:09:23 +0300

    Added  Event_Organizer_Toolkit_Accommodations_Handler::update method.

--- rest-api/models/accommodations.php
+++ rest-api/models/accommodations.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Accommodations handler of the plugin.
+ * Class contains handling CRUD related to accommodations
  *
  * @link       https://janneseppanen.site
  * @since      1.0.0
@@ -14,21 +14,72 @@
  class Event_Organizer_Toolkit_Accommodations_Handler extends Event_Organizer_Toolkit_Request_Handler {
 
     /**
-     * Handler for wp-json/event-organizer-toolkit/v1/add-accommodation endpoint
-     * 
+	 * The database table for accommodations.
+	 *
+	 * @since    1.0.0
+	 * @access   public
+	 * @var      string    $table    The database table for accommodations.
+	 */
+
+    public $table;
+
+    /**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $table       The database table for accommodations.
+	 */
+
+    public function __construct() {
+
+        global $wpdb;
+
+        $this->table = $wpdb->prefix . EVENT_ORGANIZER_TOOLKIT_ACCOMMODATIONS_TABLE;
+
+    }
+
+
+    /**
+     * Method for adding and updating an accommodation
+     *
      * @since 1.0.0
-     */   
+     */
+    public function update(WP_REST_Request $request)
+    {
+        global $wpdb;
+        global $eot_errors;
+        $eot_errors = new WP_Error();
 
-     public function add( WP_REST_Request $request ) {
+        $params = apply_filters( 'eot_json_params', $request->get_json_params() );
+    
+        // Validate parameters
+        parent::validate_required_texts( ['title'], $params );
+        parent::validate_texts( ['description'], $params );
+        parent::validate_arrays( ['rooms'], $params );        
+        
+        parent::check_errors();
+        
+        // Collect data
 
-        $json_params = apply_filters( 'eot_json_params', $request->get_json_params() );
+        $id = isset($params['id']) ? (int)$params['id'] : null;
 
-        $response = [
-            'state' => 'test add',
-        ];
+        // Sanitize and collect data
+        $data = array(
+            'title' => sanitize_text_field($params['title']),
+            'description' => isset($params['description']) ? wp_kses_post($params['description']) : '',
+            'rooms' => isset($params['rooms']) ? serialize(array_map('sanitize_text_field', $params['rooms'])) : '',
+        );
 
-        return $response;       
+        // Update if ID exists
+        if ( $id !== null ) {
 
+            parent::update_data( $this->table, $params, $data, 'Accommodation' );
+
+        } else {
+
+            parent::insert_data( $this->table, $data, 'Accommodation' );
+
+        }
     }
     
     /**
@@ -84,6 +135,5 @@
         return $response;       
 
     }
-
 
 }

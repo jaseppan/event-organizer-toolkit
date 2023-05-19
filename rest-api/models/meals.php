c9a4c22be76d9ac05e50079b0d99183b52ec33d3
Janne Seppänen <j.v.seppanen@gmail.com>
2023-05-19 13:11:59 +0300

    Added Event_Organizer_Toolkit_Meals_Handler::update method.

--- rest-api/models/meals.php
+++ rest-api/models/meals.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Meals handler of the plugin.
+ * Class contains handling CRUD related to meals
  *
  * @link       https://janneseppanen.site
  * @since      1.0.0
@@ -14,22 +14,78 @@
  class Event_Organizer_Toolkit_Meals_Handler extends Event_Organizer_Toolkit_Request_Handler {
 
     /**
-     * Handler for wp-json/event-organizer-toolkit/v1/add-accommodation endpoint
-     * 
-     * @since 1.0.0
-     */   
+	 * The database table for meals.
+	 *
+	 * @since    1.0.0
+	 * @access   public
+	 * @var      string    $table    The database table for meals.
+	 */
 
-     public function add( WP_REST_Request $request ) {
+     public $table;
 
-        $json_params = apply_filters( 'eot_json_params', $request->get_json_params() );
-
-        $response = [
-            'state' => 'test add',
-        ];
-
-        return $response;       
-
-    }
+     /**
+      * Initialize the class and set its properties.
+      *
+      * @since    1.0.0
+      * @param      string    $table       The database table for meals.
+      */
+ 
+     public function __construct() {
+ 
+         global $wpdb;
+ 
+         $this->table = $wpdb->prefix . EVENT_ORGANIZER_TOOLKIT_MEALS_TABLE;
+ 
+     }
+ 
+     /**
+      * Handler for both wp-json/event-organizer-toolkit/v1/add-meal and ../update-meal endpoints
+      * 
+      * @since 1.0.0
+      */ 
+ 
+     public function update( WP_REST_Request $request ) {
+ 
+         global $wpdb;
+         global $eot_errors;
+         $eot_errors = new WP_Error();
+ 
+         $params = apply_filters( 'eot_json_params', $request->get_json_params() );
+     
+         // Validate parameters
+         parent::validate_required_texts( [
+             'title',
+             'date',
+             'time',
+         ], $params );
+         parent::validate_texts( ['venue'], $params );       
+         
+         parent::check_errors();
+         
+         // Collect data
+ 
+         $id = isset($params['id']) ? (int)$params['id'] : null;
+ 
+         // Sanitize and collect data
+         $data = array(
+            'title' => sanitize_text_field($params['title']),
+            'date' => sanitize_text_field($params['date']),
+            'time' => sanitize_text_field($params['time']),
+            'venue' => isset($params['venue']) ? sanitize_text_field($params['venue']) : ''
+        );
+ 
+         // Update if ID exists
+         if ( $id !== null ) {
+ 
+             parent::update_data( $this->table, $params, $data, 'Event type' );
+ 
+         } else {
+ 
+             parent::insert_data( $this->table, $data, 'Event type', false );
+ 
+         }
+ 
+     }
     
     /**
      * Handler for wp-json/event-organizer-toolkit/v1/list-accommodation endpoint

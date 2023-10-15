090d04294de069097e848aed47c67f9186809b36
Janne Seppänen <j.v.seppanen@gmail.com>
2023-10-15 07:20:08 +0300

    Enhanced accommodation list view: 1) Added a dedicated area for messages, 2) Introduced an 'Items per Page' control 3) Reorganized elements for improved layout.

--- admin/partials/lists/accommodations.php
+++ admin/partials/lists/accommodations.php
@@ -1,9 +1,16 @@
 <div class="wrap">
     <h1>Accommodation List</h1>
 
-    
+    <!-- Search bar -->
+    <div class="eot-list-actions">
+        <div id="eot-found-message"></div>
+        <div class="eot-search-bar">
+            <input type="text" id="eot-search" placeholder="Search by name">
+            <button id="search-button" class="eot-list-action-button">Search</button>
+        </div>
+    </div>
 
-     <!-- Search bar -->
+     <!-- Actions bar -->
      <div class="eot-list-actions">
         <div class="eot-list-batch-actions">
             <select name="eot-batch-action-select" id="eot-batch-select">
@@ -12,16 +19,14 @@
             </select>
             <button id="eot-batch-action-submit" class="eot-list-action-button">Submit</button>
         </div>
-         <div class="eot-search-bar">
-            <input type="text" id="eot-search" placeholder="Search by name">
-            <button id="search-button" class="eot-list-action-button">Search</button>
+        <!-- Pagination links -->
+        <div class="pagination-container">
+            <!-- Pagination links will be displayed here -->
+        </div>
+         <div class="count-of-items-container">
+            <input type="text" id="count-of-items" placeholder="Count of items">
         </div>
      </div>
-
-    <!-- Pagination links -->
-    <div class="pagination-container">
-        <!-- Pagination links will be displayed here -->
-    </div>
 
     <!-- Container to display accommodation list -->
     <table id="eot-list" class="eot-table">
@@ -47,6 +52,18 @@
                 success: function(response) {
                     // Clear the previous list
                     $('#eot-list').empty();
+                    $('#eot-found-message').empty();
+
+                    // Add count of items
+                    var message = response.data.message,
+                        items_per_page = response.data.count.items_per_page;
+
+                    console.log(message);
+
+                    $('#eot-found-message').append(message);
+                    $('#count-of-items').val(items_per_page);
+
+                    // Create table header
 
                     // Create select all checkbox
                     var checkbox = $('<input></input>')
@@ -54,7 +71,6 @@
                     .attr('id', 'select-all')
                     .attr('name', 'accommodations[]');
 
-                    // Create table header
                     var header = $('<tr></tr>');
                     header.append($('<th></th>').append(checkbox));
                     header.append('<th>Actions</th>');
@@ -62,8 +78,9 @@
                     header.append('<th>Name</th>');
                     $('#eot-list').append(header);
 
-                    // Display accommodations
+                    // Display list
                     response.data.data.forEach(function(accommodation) {
+
                         // Create a list item for each accommodation
                         var listItem = $('<tr></tr>');
                         
@@ -73,8 +90,6 @@
                             .attr('name', 'accommodations[]')
                             .attr('value', accommodation.id);
                         
-                    
-
                         // Create Edit and Delete links
                         var editLink = $('<a></a>')
                             .attr('href', eotScriptData.current_url + '&tab=edit&id=' + accommodation.id)
@@ -93,6 +108,9 @@
                                 // Handle delete action here
                                 // You can use accommodation.id to identify the item to delete
                             });
+
+                        // create order number
+
 
                         listItem.append($('<td></td>').append(checkbox));
                         listItem.append($('<td></td>').append(editLink).append(deleteLink));
@@ -125,7 +143,17 @@
                         e.preventDefault();
                         var page = $(this).data('page');
                         fetchAccommodations(page);
+
+                        // Update the URL with the new page parameter
+                        updateUrlParameter('list-page', page);
                     });
+
+                    // Function to update a URL parameter
+                    function updateUrlParameter(key, value) {
+                        var url = new URL(window.location.href);
+                        url.searchParams.set(key, value);
+                        history.pushState({}, '', url.toString());
+                    }
                 },
                 error: function(xhr, status, error) {
                     console.error('Error fetching accommodations:', error);
@@ -133,10 +161,20 @@
             });
         }
 
-        // Initial fetch of accommodations on page load
-        fetchAccommodations(1);
+        // Get the list-page when the page loads
 
-        // Example: Fetch accommodations for a specific page (e.g., page 2)
-        // fetchAccommodations(2);
+        const initialListPage = getUrlParameter('list-page');
+        if (initialListPage) {
+            fetchAccommodations(initialListPage);
+        } else {
+            fetchAccommodations(1);
+        }
+
     });
+
+    // Function to get a URL parameter by name
+    function getUrlParameter(name) {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get(name);
+    }
 </script>
